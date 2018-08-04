b678fc0b49e3347ddfa75fbd20cb8478a990a643
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-08-04 00:52:56 -0300

    Program to split tokens and create dictionary added

---
+++ main.py
@@ -0,0 +1,35 @@
+import re
+
+dictionary = []
+lineCounter = 0
+delimiters = set(':;.')
+
+def main():
+    readArchive("program.txt")
+    printDictionary()
+
+def readArchive(path):
+    global lineCounter
+    with open(path, "r") as file:
+        for line in file:
+            readLine(line)
+            lineCounter = lineCounter + 1
+
+def readLine(line):
+    line = re.sub(r'{.*$', "", line) # Remove Commentaries '{}'
+    split = line.split(); # Split line into tokens
+    for word in split:
+        if any((d in delimiters) for d in word): # Dummy way to search delimiters
+            strDelimiter = word[len(word)-1] # Get delimiter string
+            dictionary.append("Token: " + word.replace(strDelimiter, "") + ", at line: " + str(lineCounter)) # Remove delimiter from word
+            dictionary.append("Token: " + strDelimiter +  " classified as: Delimiter, at line: " + str(lineCounter)) # Print delimiter
+        else: # If none delimiter was found
+            dictionary.append("Token: " + word + ", at line: " + str(lineCounter))
+
+def printDictionary():
+    global dictionary
+    for line in dictionary:
+        print line
+
+if __name__ == "__main__":
+    main()
