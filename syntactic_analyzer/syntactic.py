721ad42a1d2bc69096241e2d25471d6483f8f2b0
Thiago <thiago.filipe@lavid.ufpb.br>
2018-08-31 15:14:02 -0300

    Add some routines

--- syntactic_analyzer/syntactic.py
+++ syntactic_analyzer/syntactic.py
@@ -1,10 +1,21 @@
 WORD = 0
 CLASSIFICATION = 1
+LINE = 2
+MISSING_ARG = -1
+MISSING_ID = -2
 
 
 class Syntactic:
     def __init__(self, lexical_input=['token','classification',1]):
         self.lexical_input = lexical_input[::-1]
+
+    def _show_error(self,error_type=0 , token = []):
+        if error_type == MISSING_ARG:
+            print('Missing argument ' + token[WORD] + ' at line ' + int(token[LINE]) + '.')
+        elif error_type == MISSING_ID:
+            print('Missing identifier following \'program\'...')
+        print('Exiting program...')
+        exit(0)
 
     def _get_next_token(self):
         if len(self.lexical_input) == 0:
@@ -13,6 +24,9 @@ class Syntactic:
 
     def _get_word(self, token=[]):
         return token[WORD]
+
+    def _get_line(self, token=[]):
+        return token[LINE]
 
     def _get_classification(self, token=[]):
         return token[CLASSIFICATION]
@@ -25,6 +39,52 @@ class Syntactic:
                 break
 
             print(self._get_classification(token) + ' ' + self._get_word(token))
+
+    def _program_routine(self):
+        token = self._get_next_token()
+        if token and self._get_word(token) == 'program':
+            token = self._get_next_token()
+            if token and self._get_classification(token) == 'identifier':
+                token = self._get_next_token()
+                if token and self._get_word(token) == ';':
+                    # checking now for possible declarations
+                    token = self._get_next_token()
+                    if token and self._get_word(token) == 'var':
+                        self._variables_declaration_routine()
+
+                    # todo subprograms declaration routine
+
+                    if token and self._get_word(token) == 'begin':
+                else:
+                    self._show_error(MISSING_ARG, token)
+            else:
+                self._show_error(MISSING_ID, token)
+        else:
+            self._show_error(MISSING_ARG, token)
+
+
+    def _variables_declaration_routine(self):
+        token = self._get_next_token()
+        if token and self._get_classification(token) == 'identifier':
+            token = self._get_next_token()
+            if token and self._get_word(token) == ':':
+                self._type_routine()
+                token = self._get_next_token()
+                if token and self._get_word(token) == ';':
+                    self._variable_declaration_subroutine()
+                else:
+                    self._show_error(MISSING_ARG,token)
+            else:
+                self._show_error(MISSING_ARG,token)
+
+    def _variable_declaration_subroutine(self):
+        
+
+        
+    def _identifiers_list_routine(self):
+
+
+    def _type_routine
 
 
 
